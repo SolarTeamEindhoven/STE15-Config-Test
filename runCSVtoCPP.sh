c0320161c82dc81d4c212832c1a509fd12c6f2ba
Xander Houtman <xander.houtman@solarteameindhoven.nl>
2015-03-29 18:46:38 +0200

    Fixed generation script bug.

--- runCSVtoCPP.sh
+++ runCSVtoCPP.sh
@@ -1,3 +1,3 @@
 cd "$( dirname "$0" )"
-java -jar ./ste15-csv2cpp/dist/CANRecordCodeGenerator.jar -i CANMessageRecord.csv -o ./ESLib/include/common
+java -jar ./ste15-csv2cpp/dist/CANRecordCodeGenerator.jar -i ./CANMessageRecord/CANMessageRecord.csv -o ./ESLib/include/common
 
